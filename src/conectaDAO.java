7ca1146f59474541285558af742f916ff8f298e9
cucato97 <gabrielcucato@icloud.com>
2025-04-06 13:43:17 -0300

    Botão de cadastrar

---
+++ src/conectaDAO.java
@@ -0,0 +1,32 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Adm
+ */
+public class conectaDAO {
+
+    public Connection connectDB() {
+        Connection conn = null;
+
+        try {
+            String url = "jdbc:mysql://localhost:3306/uc11"; 
+            String user = "root"; 
+            String password = "root"; 
+
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException erro) {
+            System.out.println("Erro ao conectar: " + erro.getMessage());
+        }
+
+        return conn;
+    }
+}
