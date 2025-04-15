19f0722170c6d7751912efc051297b2f38703876
cucato97 <gabrielcucato@icloud.com>
2025-04-14 22:23:54 -0300

    Tela Vendidos

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,25 +4,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> c718997142311c5294ed30ab2f6c62decce5f52a
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-<<<<<<< HEAD
-=======
-
->>>>>>> c718997142311c5294ed30ab2f6c62decce5f52a
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
-<<<<<<< HEAD
+
 
     public Connection connectDB() {
         Connection conn = null;
@@ -39,20 +24,9 @@ public class conectaDAO {
 
         return conn;
     }
-=======
     
-    public Connection connectDB(){
-        Connection conn = null;
-        
-        try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
-        }
-        return conn;
-    }
+
     
->>>>>>> c718997142311c5294ed30ab2f6c62decce5f52a
+ 
+
 }
